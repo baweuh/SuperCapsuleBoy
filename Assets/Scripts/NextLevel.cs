6490abed79ccd43ee64d3acaffb789943a90dc60
Sebastienliu1 <sebastienliu1105@gmail.com>
2024-01-11 16:21:42 +0100

    modification du script NextLevel

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+
+    [SerializeField] int nextLevel = 1;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            SceneManager.LoadScene("Level2");
+            // Utilisez la variable nextLevel dans le nom du niveau
+            string nextLevelName = "Level" + nextLevel.ToString();
+            SceneManager.LoadScene(nextLevelName);
         }
     }
+
 }
